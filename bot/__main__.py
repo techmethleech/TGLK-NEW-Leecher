a4d7cffce89958771ad6037c2738dac57454e805
techmethleech <100306770+techmethleech@users.noreply.github.com>
2022-07-15 08:05:14 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,17 +65,17 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("TechGuideLK", "https://t.me/TechGuideLKChannel")
-    buttons.buildbutton("Movie Channel", "https://t.me/slmovietvseries")
+    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
+    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-ඕනෑම drect link එකක් leech කරන්න පුළුවන්!
+This bot can mirror all your links to Google Drive or to telegram!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('මෙම bot භාවිතා කිරීමට TechGuideLK සම්බන්ධ කරගන්න', context.bot, update.message, reply_markup)
+        sendMarkup('Not an Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
